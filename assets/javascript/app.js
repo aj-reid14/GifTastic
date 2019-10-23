f662e9f252c3844690b8039c9ad1ace2d4b05537
ajreid <axr490@miami.edu>
2019-10-22 23:43:28 -0400

    started functionality for ajax reuests on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,36 @@
-let topics = ["Animals", "Insects", "Cars", "Colors"];
+let topics = ["animals", "insects", "cars", "colors"];
+let API_Key = "ty0kxw0Ygk6F1hpxthRk0qLTJfikt2D7";
+let search;
+let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + "" + "&api_key=" + API_Key + "&limit=10";
 
 $(document).ready(function()
 {
-
-    for (let i = 0; i < topics.length; i++)
-    {        
+    for (let i = 0; i < topics.length; i++) 
+    {
         let newTopicButton = $("<button>");
-        newTopicButton.text(topics[i]);
+        newTopicButton.addClass("topic-button");
+        newTopicButton.val(topics[i]);
+        newTopicButton.text(topics[i].charAt(0).toUpperCase() + topics[i].substring(1));
         $("#button-row").append(newTopicButton);
     }
 
+   ConfigureButtons();
+
 })
+
+function ConfigureButtons()
+{
+    $(".topic-button").click(function () 
+    {
+        search = $(this).val();
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + API_Key + "&limit=10";
+
+        $.ajax({
+            URL: queryURL,
+            method: "GET"
+        }).then(function (response) 
+        {
+            console.log(response);
+        })
+    })
+}
